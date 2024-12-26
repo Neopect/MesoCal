dd6b9a99dd0c468e73ab1c4003513053d5979a74
Tyler <tyler.moen@earthlink.net>
2024-12-25 19:40:44 -0600

    Basic start. Is able to take input and produce a set output, nothing more

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # MesoCal
+
+MesoCal will be designed as a Macrocycle builder for scheduling and weight forecasting. The plan is to make it dynamic between custom mesocycles and adjust throughout your macrocycle.
+
+## State
+
+The state of this script/app is fundamental. I haven't spent much time expanding feature sets or making them more stable and user-friendly. It currently follows a strict process to function.
+
+
+## How to use the application?
+
+To run this script, you need to have 2 things ready, the script and your adequately formatted JSON file within your working directory. Then follow the following prompts:
+1. Type 2 for `2. Build Macrocycle`
+2. Type 1 for JSON
+3. Type the assigned number for the designated file
+4. Your output.csv file will be saved to your cwd
+
+Everything else on this application so far are placeholders and/or don't serve any function at this time. They will be removed or built upon as time and progress permits.
+
+## JSON formatting
+I have provided a sample JSON file as an example of how to base custom macrocycles.
+
+Here's the code snip-it:
+```
+{
+    "name":"",
+    "startDate":"1/1/25",
+    "startingWeight":200,
+    "cycleNumberPrefix":"",
+    "mesocycles": [
+        {"name": "", "weeks": 4, "deload": [4], "glmrStat": "", "rate":0.25},
+        {"name": "", "weeks": 2, "deload": [], "glmrStat": "", "rate":0}
+    ],
+    "manualWeights": [
+        {"week":2,"weight":205}
+    ]
+}
+```
+
+You can have as many mesocycles and manual weight entries as possible. Still, you need at least one mesocycle for the script to function. 
+
+`startDate` - Represents start date for your Macrocycle (Must be a Monday for formatting reasons)
+
+`startingWeight`- Your starting weight
+
+`cycleNumberPrefix` - Mesocycles are numbered 1-9+. If you want to create a prefix like `1.`, it will output like `[1.1, 1.2, 1.3]`. This can be helpful for documentation reasons when you're storing multiple Macrocycles or variations of them to stay better organized.
+
+`mesocycles` - List of your mesocycles
+
+`{"name": "", "weeks": 4, "deload": [4], "glmrStat": "", "rate":0.25}`
+- `name` is your meso's name
+- `weeks` is how many total weeks within meso
+- `deload` is a list of each deload week within your mesocycle. Each number on the list will be marked as a deload, so set your gain rate to 0 for that week. If your meso doesn't have a deload, you can leave it empty
+- `glmrStat` is meant to represent the type of cycle, it serves no purpose atm and is WIP
+- `rate` is your gain/lose rate for body weight. The number is taken as a percentage. In this example, `0.25` means you plan to gain 0.25% body weight per week
+
+
+## Future Feature/Change Ideas
+- Build a more stable interface
+- More code formatting more consistent
+- Be able to manually create macrocycles without json using prompts
+- Multiple output formats (printf, markdown, pdf, xlsx)
+- More manipulation and options in output
+- Display Macrocycle Analytics 
---
+++ src/base/core.py
@@ -0,0 +1,184 @@
+import os
+import glob
+import json
+import csv
+from datetime import datetime, timedelta
+
+def buildMacroToCSV(data, inGCal = True):
+
+    # Parse the start date
+    start_date = datetime.strptime(data["startDate"], "%m/%d/%y")
+
+    # Initialize variables
+    current_weight = data["startingWeight"]
+    current_date = start_date
+
+    # Parse cycle prefix numbering scheme
+    cycle_number_prefix = data["cycleNumberPrefix"]
+    cycle_number = 1
+
+    # Macrocycle week counter
+    macro_week = 1
+
+    # Create a list to store the CSV rows
+    csv_rows = []
+
+    # Iterate through each mesocycle
+    for cycle in data["mesocycles"]:
+        cycle_name = cycle["name"]
+        weeks = cycle["weeks"]
+        deload_weeks = cycle["deload"]
+        rate = cycle["rate"]
+
+        # Iterate through each week in the mesocycle
+        for week in range(1, weeks + 1):
+            # Check if the current week is a deload week
+            if week in deload_weeks:
+                week_number = f"{week}D"
+            else:
+                week_number = str(week)
+            
+            # Check if there is a manual weight for the current week
+            manual_weight_entry = next((entry for entry in data["manualWeights"] if entry["week"] == macro_week), None)
+            if manual_weight_entry:
+                current_weight = manual_weight_entry["weight"]
+            
+            # Check if cycle week needs it's name printed in notes
+            if week != 1:
+                cycle_name = ""
+
+
+            # Creates microcycle row
+            microcycle_row = [cycle_number_prefix+str(cycle_number),week_number, rate, round(current_weight, 2), cycle_name, macro_week]
+
+            # Creates the weekly calendar for the said week based on timedelta
+            if inGCal == True:
+                microcycle_row.append(current_date.strftime("%b"))
+                for day in range(0,7):
+                    microcycle_row.append((current_date + timedelta(days=day)).strftime("%-d"))
+
+
+            # Append the row to the CSV rows list
+            csv_rows.append(microcycle_row)
+
+            
+            # Update the weight for the next week if it's not a deload week
+            if week not in deload_weeks:
+                current_weight += current_weight * (rate/100)
+            
+            # Update the date for the next week
+            current_date += timedelta(weeks=1)
+
+            # Update the week number
+            macro_week += 1
+
+        # Update cycle count
+        cycle_number +=1
+
+    macrocycle_headerrow = ["Cycle", "Cycle Week", "Gain (%)", "Weight Predictions", "Notes", "Week"]
+    if inGCal == True:
+        macrocycle_headerrow.extend(['Month','Mon','Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'])
+
+
+    # Write the CSV rows to a file
+    with open("output.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(macrocycle_headerrow)
+        writer.writerows(csv_rows)
+
+    
+    print("CSV file 'output.csv' has been created successfully.")
+
+def main():#
+    print("Test complete")
+
+def listMacros():
+    """
+    Scan all files in cwd and find any applicable "xxx.mesocal.json" files
+    FIXME: allow to insert file paths
+    Open each file and load the names of each
+    """
+    # Get the current working directory
+    cwd = os.getcwd()
+    print(cwd)
+
+    # Use glob to find all files with ".mesocal.json" in their name
+    files = glob.glob(os.path.join(cwd, '*mesocal.json'))
+
+    # Print the list of files
+    x = 1
+    for file in files:
+        print(str(x)+". "+str(file))
+        x+=1
+
+    try:
+        selection = int(input('Please select option: '))
+    except:
+        print('Invalid Option')
+
+    return files[selection-1]
+    
+
+def viewMacro():
+    print('Sorry, this is WIP')
+
+
+def buildMacro():
+    # Build Macrocycle
+
+    print("\n\nOptions:")
+    print("1. Build from JSON file")
+    print("2. Build manually (WIP/Not Built)")
+
+    try:
+        command = int(input("Please type an option: "))
+        if command == 1:
+            macroSample = listMacros()
+            print(macroSample)
+            file = open(macroSample)
+            jsonData = json.load(file)
+            file.close()
+            buildMacroToCSV(jsonData)
+    except:
+        raise Exception("Error has occured")
+
+def editMacro():
+    print('Sorry, this is WIP')
+
+def delMacro():
+    print('Sorry, this is WIP')
+
+
+def menu():
+    # Main menu screen
+    while True:
+        print("\n\nWelcome to MesoCal!")
+        print("1. View Macrocycle")
+        print("2. Build Macrocycle")
+        print("3. Edit Macrocycle")
+        print("4. Delete Macrocycle")
+        print("0. Exit")
+        
+        try:
+            command = int(input("Please type an option: "))
+
+            if command == 1:
+                viewMacro()
+            elif command == 2:
+                buildMacro()
+            elif command == 3:
+                editMacro()
+            elif command == 4:
+                delMacro()
+            elif command == 0:
+                break
+            else:
+                raise Exception("Not an option")
+        except:
+            print("Please select valid option")
+        
+        
+
+
+
+menu()
